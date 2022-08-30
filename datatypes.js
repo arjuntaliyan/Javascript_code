7a9ecc83c7fca2a2d0d448c63905da2f0f7ab0fa
Arjun Taliyan <105619051+arjuntaliyan@users.noreply.github.com>
2022-08-30 23:07:47 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,38 @@
+/*
+
+// OBJECTS
+
+let animal  = {
+    name : "Cow",
+    legs : 4
+}
+
+// Dot Notation
+console.log(animal.name);
+// Bracket Notation
+console.log(animal["legs"]);
+
+*/
+
+/*
+
+// ARRAYS
+
+let selectBooks = ["The Rudest book Ever", "Atomic Habits", "Rich Dad Poor Dad"]
+ console.log(selectBooks);
+
+*/
+
+// FUNCATION
+
+function namasteWorld(name) {
+    console.log("Namaste " + name);
+}
+
+namasteWorld("Arjun")
+
+function addition(a,b) {
+    return a+b;
+}
+
+console.log(addition(5,6));
