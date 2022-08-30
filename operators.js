ef36bab20c23e73e62b2f9adae8c6b9df3742037
Arjun Taliyan <105619051+arjuntaliyan@users.noreply.github.com>
2022-08-30 23:10:19 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,21 @@
+var x = 52, y = 10;
+x = x%y;
+
+var a= 10, b=2;
+a = a**b;
+ 
+
+var x= 3 ==='3';
+x
+
+var x = 7;
+x--;
+x
+
+var x = true;
+x = +x;
+x
+
+var x = "Namaste" , y = "World";
+x = x + " " + y;
+x
---
+++ strings.js
@@ -0,0 +1,14 @@
+var favShow = "Game of thrones";
+console.log(favShow.length)
+
+console.log(favShow[0]);
+
+console.log(favShow[favShow.length-1])
+
+console.log(favShow.indexOf("of"))
+
+console.log(favShow.slice(5,7));
+
+var srk = "My Name is KhaN";
+console.log(srk.toUpperCase());
+console.log(srk.toLocaleLowerCase());
