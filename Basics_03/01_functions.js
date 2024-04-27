0abbd5d01d2d9dd09055469b94719407d687e6e1
Arjun Taliyan <105619051+arjuntaliyan@users.noreply.github.com>
2024-04-27 17:50:08 +0000

    function in {} & []

--- Basics_03/01_functions.js
+++ Basics_03/01_functions.js
@@ -24,10 +24,44 @@ const result = addTwoNumbers (3,5) // arguments
 
 function loginUserMessage(username) {
     if(!username){ //username === undefined
-        console.log("PLease enter a username");
+        // console.log("PLease enter a username");
         return
     }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage()) // if its empty it return undefined
+// console.log(loginUserMessage()) // if its empty it return undefined
+
+
+function calculateCartPrice(...num1) { // rest operator or spread opreator
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam", // object in function
+    price: 399
+})
+
+const myNewArray = [200, 400, 600, 800]
+
+function returnSecondValue(getArray) { // Array in function
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 600, 800]));
